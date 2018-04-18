47c9b109bf7fd25983a080986338fbc904783620
Ubuntu1710 <renan.salazar@gmail.com>
2018-04-17 22:10:38 -0300

    Conexão Com banco refatorado

---
+++ app/infra/connectionFactory.js
@@ -0,0 +1,11 @@
+var mysql = require('mysql');
+
+module.exports = function () {
+
+    return mysql.createConnection({
+        host: 'roldanainstance.cqunr0spvtoh.us-east-1.rds.amazonaws.com',
+        user: 'root',
+        password: 'Pa$$4eit',
+        database: 'roldanaDB'
+    });
+}
--- app/routes/catalogo.js
+++ app/routes/catalogo.js
@@ -1,19 +1,17 @@
+var connectionFactory = require('../infra/connectionFactory');
+
 module.exports = function (app) {
     app.get('/catalogo', function (req, res) {
 
-        var mysql = require('mysql');
-        var connection = mysql.createConnection({
-            host:'roldanainstance.cqunr0spvtoh.us-east-1.rds.amazonaws.com',
-            user:'root',
-            password:'Pa$$4eit',
-            database:'roldanaDB'
-        });
+        var connection = connectionFactory();
 
-        connection.query('SELECT * FROM roldanas', function(error, results){
+        connection.query('SELECT * FROM roldanas', function (error, results) {
 
-            if(error) throw error;
+            if (error) throw error;
             // console.log('conexão com o banco deu certo', results);
-            res.render('roldanas/catalogo',{resultsHTML:results});
+            res.render('roldanas/catalogo', {
+                resultsHTML: results
+            });
         });
         connection.end();
     });
